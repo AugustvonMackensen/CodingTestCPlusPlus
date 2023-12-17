c2db63555c0638eaef71d8a1d2d49a9c92e9306a
Leo Lee <basil1452@protonmail.com>
2023-12-17 16:44:58 +0900

    초기 커밋.

---
+++ CodingTest/CodingTest.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    string oddNum;
+    string evenNum;
+
+    for (int num : num_list) {
+        if (num % 2 == 0) {
+            //TODO : to_string() function must be used to add number on String
+            evenNum += to_string(num);
+        }
+        else {
+            oddNum += to_string(num);
+        }
+    }
+
+    answer = stoi(evenNum) + stoi(oddNum);
+    return answer;
+};
+
+
+int main()
+{
+    std::cout << solution({ 3,4,5,2,1 }) << endl;
+    std::cout << solution({5,7,8,3}) << endl;
+}
